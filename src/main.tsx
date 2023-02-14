ab1bb83423c9f43c4889930e5e728eb3830fe624
Sunset-kim <wood2002@naver.com>
2023-02-14 17:37:21 +0900

    build: setup @tanstack/react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,27 @@
+import { router } from '@/feature/router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { router } from '@/feature/router';
 import { SearchServiceProvider } from './feature/search/search.context';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      staleTime: 1000 * 60 * 5,
+      cacheTime: 1000 * 60 * 10,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <SearchServiceProvider>
-      <RouterProvider router={router} />
-    </SearchServiceProvider>
+    <QueryClientProvider client={queryClient}>
+      <SearchServiceProvider>
+        <RouterProvider router={router} />
+      </SearchServiceProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
