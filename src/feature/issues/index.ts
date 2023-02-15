a5ed3f9883d48ad53eb4ca8e84584adb2f48019c
Sunset-kim <wood2002@naver.com>
2023-02-15 19:47:38 +0900

    fix: replace name & convert schema

--- src/feature/issues/index.ts
+++ src/feature/issues/index.ts
@@ -3,4 +3,4 @@ export * from './issue-service.context';
 export * from './issue.service';
 export * from './issues.query';
 export * from './schema';
-export * from './use-Issues';
+export * from './use-Issues-query';
--- src/feature/issues/use-Issues-query.ts
+++ src/feature/issues/use-Issues-query.ts
@@ -1,14 +1,23 @@
 import { useQueries } from '@tanstack/react-query';
 import { useIssueService } from './issue-service.context';
 import { issuesCache } from './issues.query';
+import { Issue } from './schema';
 
-export const useIssues = (nameList: string[]) => {
+export const useIssuesQuery = (nameList: string[]) => {
   const { service } = useIssueService();
 
   const issuesQueries = useQueries({
     queries: nameList.map((name) => ({
       queryKey: issuesCache.getIssue({ name }),
       queryFn: () => service.getIssues({ repositoryName: name }),
+      select: (response: Issue[]) => {
+        return response.map(
+          (data) =>
+            ({ ...data, repositoryName: name } as Issue & {
+              repositoryName: string;
+            })
+        );
+      },
     })),
   });
 
