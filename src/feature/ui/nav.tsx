17f393407e00e2c4c4cb77fe048f9ad1f55cbb79
Sunset-kim <wood2002@naver.com>
2023-02-17 01:02:14 +0900

    fix: bug - tab activation does not match page path

--- src/feature/ui/nav.tsx
+++ src/feature/ui/nav.tsx
@@ -1,17 +1,33 @@
 import { Tab, TabList, Tabs } from '@chakra-ui/react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+
+const NAV_LIST = [
+  {
+    path: '/',
+    title: 'Search',
+  },
+  {
+    path: '/issues',
+    title: 'Issues',
+  },
+];
 
 export function Nav() {
+  const { pathname } = useLocation();
+
   return (
     <nav>
-      <Tabs isFitted variant="enclosed">
+      <Tabs
+        index={NAV_LIST.findIndex(({ path }) => path === pathname)}
+        isFitted
+        variant="enclosed"
+      >
         <TabList>
-          <Tab as={Link} to={'/'}>
-            Search
-          </Tab>
-          <Tab as={Link} to={'/issues'}>
-            Issues
-          </Tab>
+          {NAV_LIST.map(({ path, title }) => (
+            <Tab key={path + title} as={Link} to={path}>
+              {title}
+            </Tab>
+          ))}
         </TabList>
       </Tabs>
     </nav>
