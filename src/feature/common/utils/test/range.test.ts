d4a5e85f05fbc83ef1414de064fa8faad041b24c
Sunset-kim <wood2002@naver.com>
2023-02-17 12:54:14 +0900

    test: add edge test case

---
+++ src/feature/common/utils/test/range.test.ts
@@ -0,0 +1,20 @@
+import 'jest';
+import { range } from '../range';
+
+describe('range', () => {
+  it('same case', () => {
+    const result = range(1, 1);
+    expect(result).toEqual([1]);
+  });
+
+  it('reverse case', () => {
+    const result = range(4, 1);
+    console.log(result);
+    expect(result).toEqual([1, 2, 3, 4]);
+  });
+
+  it('nomal case', () => {
+    const result = range(1, 4);
+    expect(result).toEqual([1, 2, 3, 4]);
+  });
+});
